fc86198de4e218fea1c628a10f5fd5fb6df86b0e
pzhang <zhangp@133.cn>
2021-05-27 11:49:17 +0800

    vite配置服务

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,24 @@ export default defineConfig({
 			routes: resolve('src/routes'),
 			styles: resolve('src/styles')
 		}
+	},
+  // 配置服务
+  server: {
+		//服务器主机名
+		host: '',
+		//端口号
+		port: 3088,
+		//设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口
+		strictPort: false,
+		//服务器启动时自动在浏览器中打开应用程序,当此值为字符串时，会被用作 URL 的路径名
+		open: false,
+		//自定义代理规则
+		proxy: {
+			'/api': {
+				target: 'http://jsonplaceholder.typicode.com',
+				changeOrigin: true,
+				rewrite: path => path.replace(/^\/api/, '')
+			}
+		}
 	}
 })
